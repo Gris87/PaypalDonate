34aa3578157043f2ac29b7017d270acb71f535f0
Gris87 <Gris87@yandex.ru>
2014-04-24 23:40:41 +0400

    Well done

--- Assets/Scripts/PaypalDonate/PaypalDonate.cs
+++ Assets/Scripts/PaypalDonate/PaypalDonate.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class PaypalDonate : MonoBehaviour
 {
 	public Texture2D buttonImage;
-	public float     screenX=0.5f;
-	public float     screenY=0.5f;
+	public float     screenX = 0.5f;
+	public float     screenY = 0.5f;
+	public int       width   = 92;
+	public int       height  = 26;
 	public string    buttonID;
+
+	private GUIStyle buttonStyle;
 
 	// Use this for initialization
 	void Start ()
@@ -15,11 +19,21 @@ public class PaypalDonate : MonoBehaviour
 		{
 			Debug.LogError("It is required to set donate button ID to use Paypal button");
 			enabled=false;
+
+			return;
 		}
+
+		buttonStyle=new GUIStyle();
+
+		buttonStyle.normal.background=buttonImage;
 	}
 
 	void OnGUI()
 	{
-	
+		// It will open link https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=buttonID
+		if (GUI.Button(new Rect(Screen.width*screenX - width*0.5f, Screen.height*screenY - height*0.5f, width, height), "", buttonStyle))
+		{
+			Application.OpenURL("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id="+buttonID);
+		}
 	}
 }
