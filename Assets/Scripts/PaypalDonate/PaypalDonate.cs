27c5ec7eb0de8943e96acc0d347dff9a9eb214cf
Gris87 <Gris87@yandex.ru>
2014-04-24 23:29:21 +0400

    Donate script

---
+++ Assets/Scripts/PaypalDonate/PaypalDonate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaypalDonate : MonoBehaviour
+{
+	public Texture2D buttonImage;
+	public float     screenX=0.5f;
+	public float     screenY=0.5f;
+	public string    buttonID;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (buttonID.Equals(""))
+		{
+			Debug.LogError("It is required to set donate button ID to use Paypal button");
+			enabled=false;
+		}
+	}
+
+	void OnGUI()
+	{
+	
+	}
+}
